3630f59d7c637e1afab7978df08a0287a12e2107
julio-cesar-dos <168609357+julio-cesar-dos@users.noreply.github.com>
2024-05-12 20:32:27 -0400

    Add files via upload

--- js/fbevents.js
+++ js/fbevents.js
@@ -17,7 +17,7 @@
 * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-fbq.version="2.9.155";
+fbq.version="2.9.156";
 fbq._releaseSegment = "stable";
 fbq.pendingConfigs=["global_config"];
 fbq.__openBridgeRollout = 1.0;
